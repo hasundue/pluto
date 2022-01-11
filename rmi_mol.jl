87f0fe956abac1d002a755f1f5311e857fc87d51
hasundue <hasundue@gmail.com>
2022-01-11 09:02:11 +0000

    Deploying to gh-pages from  @ 4d9992ea4fc93026e30fadee60e39638e6c61cd2 🚀

--- rmi_mol.jl
+++ rmi_mol.jl
@@ -4,6 +4,28 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 421056b1-5fd8-4a43-b9f9-29ab6655f01e
+begin
+    import Pkg
+    Pkg.activate(mktempdir())
+
+    # Registered packages
+    pkgnames = ["ModelingToolkit",
+                "OrdinaryDiffEq",
+                "SpecialFunctions",
+                "Plots",
+                "Printf",
+                "RecipesBase"]
+    pkgspecs = [Pkg.PackageSpec(name = pkgname) for pkgname in pkgnames]
+    Pkg.add(pkgspecs)
+
+    # Unregistered packages
+    Pkg.add(Pkg.PackageSpec(url="https://github.com/SciML/MethodOfLines.jl"))
+
+    using ModelingToolkit, SpecialFunctions, MethodOfLines, OrdinaryDiffEq
+    using Plots, RecipesBase, Printf
+end
+
 # ╔═╡ 43cd59d9-ad46-42d7-a7ba-443a2bd4f8de
 @parameters t x
 
@@ -48,19 +70,19 @@ cp(s) = 1 + erf((s-1)/w) # relative capillary pressure
 
 # ╔═╡ ca6af1e9-2bf5-4751-aa95-8b685f4bb987
 eqs = [
-	Dt(c(t,x)) ~ D * Dxx(c(t,x)) - δ * cg(c(t,x), s(t,x)) + ev(c(t,x), u(t,x)),
-	Dt(s(t,x)) ~ cg(c(t,x), s(t,x)),
-	Dt(u(t,x)) ~ Dx(k*cp(s(t,x))*Dx(u(t,x))),
+    Dt(c(t,x)) ~ D * Dxx(c(t,x)) - δ * cg(c(t,x), s(t,x)) + ev(c(t,x), u(t,x)),
+    Dt(s(t,x)) ~ cg(c(t,x), s(t,x)),
+    Dt(u(t,x)) ~ Dx(k*cp(s(t,x))*Dx(u(t,x))),
 ]
 
 # ╔═╡ b084b8bd-2fe6-4d38-9090-34613ef41842
 bcs = [
-	c(t,0) ~ 1.0,
-	c(0,x) ~ 0.0,
-	s(t,0) ~ 1.0,
-	s(0,x) ~ 0.0,
-	u(t,0) ~ 1.0,
-	u(0,x) ~ 0.0,
+    c(t,0) ~ 1.0,
+    c(0,x) ~ 0.0,
+    s(t,0) ~ 1.0,
+    s(0,x) ~ 0.0,
+    u(t,0) ~ 1.0,
+    u(0,x) ~ 0.0,
 ]
 
 # ╔═╡ 25ad6ce3-86fc-478f-b957-66857422e2b9
@@ -84,72 +106,47 @@ sol = solve(prob, Tsit5());
 
 # ╔═╡ 93588680-d150-47d1-8cde-8f2c39441881
 @recipe function plot(xs::StepRangeLen, sol::ODESolution)
-	ts = sol.t
-	N = length(ts)
-	xs = collect(xs[2:end])
-	M = length(xs)
+    ts = sol.t
+    N = length(ts)
+    xs = collect(xs[2:end])
+    M = length(xs)
 
-	layout := @layout [c s
-                           u u]
+    layout := @layout [c s
+                       u u]
 
-	tout = collect(0:0.2:1)
-	for i in 1:N
-		if ts[i] ≥ tout[1]
-			tout = tout[2:end]
-			@series begin
-				subplot := 1
-				ylabel --> "gas"
-				xlabel --> "x"
-				label --> @sprintf "t = %1.1f" ts[i]
-				xs, sol.u[i][1:M]
-			end
-			@series begin
-				subplot := 2
-				ylabel --> "coverage"
-				xlabel --> "x"
-				label --> @sprintf "t = %1.1f" ts[i]
-				xs, sol.u[i][M+1:2M]
-			end
-			@series begin
-				subplot := 3
-				ylabel --> "liquid"
-				xlabel --> "x"
-				label --> @sprintf "t = %1.1f" ts[i]
-				xs, sol.u[i][2M+1:3M]
-			end
-		end
-	end
-	xlims --> (0, 1)
-	ylims --> (0, 1)
+    tout = collect(0:0.2:1)
+    for i in 1:N
+        if ts[i] ≥ tout[1]
+            tout = tout[2:end]
+            @series begin
+                subplot := 1
+                ylabel --> "gas"
+                xlabel --> "x"
+                label --> @sprintf "t = %1.1f" ts[i]
+                xs, sol.u[i][1:M]
+            end
+            @series begin
+                subplot := 2
+                ylabel --> "coverage"
+                xlabel --> "x"
+                label --> @sprintf "t = %1.1f" ts[i]
+                xs, sol.u[i][M+1:2M]
+            end
+            @series begin
+                subplot := 3
+                ylabel --> "liquid"
+                xlabel --> "x"
+                label --> @sprintf "t = %1.1f" ts[i]
+                xs, sol.u[i][2M+1:3M]
+            end
+        end
+    end
+    xlims --> (0, 1)
+    ylims --> (0, 1)
 end
 
 # ╔═╡ eac0b5d5-866d-4a59-b33a-e81eb5c11a4e
 plot(0:dx:1, sol)
-
-# ╔═╡ 60efe4f1-c5f5-48bd-a79a-9cb7b5e85b7a
-using SpecialFunctions
-
-# ╔═╡ 421056b1-5fd8-4a43-b9f9-29ab6655f01e
-begin
-    import Pkg
-    Pkg.activate(mktempdir())
-
-    # Registered packages
-    pkagnames = ["ModelingToolkit",
-		 "OrdinaryDiffEq",
-		 "SpecialFunctions",
-		 "Plots",
-		 "Printf",
-		 "RecipesBase"]
-    pkgspecs = [Pkg.PackageSpec(name = pkgname) for pkgname in pkgnames]
-    Pkg.add(pkgspecs)
-
-    # Unregistered packages
-    Pkg.add(Pkg.PackageSpec(url="https://github.com/SciML/MethodOfLines.jl"))
-
-    using ModelingToolkit, SpecialFunctions, MethodOfLines, OrdinaryDiffEq
-    using Plots, RecipesBase, Printf
-end
 
 # ╔═╡ Cell order:
 # ╠═421056b1-5fd8-4a43-b9f9-29ab6655f01e
@@ -164,7 +161,6 @@ end
 # ╠═7af0447f-aca2-45b0-ac1c-5d359ec25b3a
 # ╠═85271f34-f54c-4f94-be61-46db68d4be3c
 # ╠═56c4d260-3d8d-4c83-8f5a-283878c76d9b
-# ╠═60efe4f1-c5f5-48bd-a79a-9cb7b5e85b7a
 # ╠═ca6af1e9-2bf5-4751-aa95-8b685f4bb987
 # ╠═84ff092f-bde9-4696-91c9-5a5b97a20cde
 # ╠═f9d3a857-5fca-4329-ad6e-3a0ab4a53331
